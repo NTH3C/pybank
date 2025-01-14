46dae9f65eb1f727748911dfebefc920f0ce19fd
Nathan <nathan@codings-MacBook-Pro.local>
2025-01-14 14:13:35 +0100

    Login WIP

--- Class/user.py
+++ Class/user.py
@@ -1,9 +1,16 @@
 import database
 
 from fastapi import FastAPI, Depends, APIRouter
-from sqlmodel import Session, create_engine, Field, SQLModel
+from sqlmodel import Session, create_engine, Field, SQLModel, select
 from pydantic import BaseModel
 from passlib.context import CryptContext
+
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
 
 router = APIRouter(tags=["Users"])
 
@@ -20,8 +27,6 @@ def get_password_hash(password):
 
 #*--------- Class ----------#
 
-db = Depends(database.get_session)
-
 class User(SQLModel, BaseModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     email: str = Field(index=True, unique=True)
@@ -29,15 +34,37 @@ class User(SQLModel, BaseModel, table=True):
 
 #*--------- Authentication ----------#
 
+def get_user(email: str, session: Session):
+    statement = select(User).where(User.email == email)  # if user email exists
+    result = session.exec(statement).first()  # fetch first result
+    return result
+
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password) # compare plain and hashed pwd
+
+def authenticate_user(email: str, password: str, session: Session):
+    user = get_user(email, session)
+    if not user:  # case user doesn't exist
+        return False
+    if not verify_password(password, user.password):  # case pwd not correct
+        return False
+    return user
 
 #*--------- App post ----------#
 
 @router.post("/users/")
-def create_user(body: User, session = db) -> User:
+def create_user(body: User,  session: Session = Depends(database.get_session)) -> User:
     user = User(email=body.email, password=get_password_hash(body.password))
     session.add(user)
     session.commit()
     session.refresh(user)
     return user
+
+@router.post("/login/")
+def login(email: str, password: str, session: Session = Depends(database.get_session)):
+    user = authenticate_user(email, password, session)
+    if not user:
+        return {"error": "Invalid credentials"}
+    return {"message": "Login successful", "user": user}
 
 #*--------- App Get ----------#
