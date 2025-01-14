f948869ea9f9163500eff438c1b350dafd1c09df
H_Khalid__H <khalidjj78200@icloud.com>
2025-01-14 22:56:23 +0100

    story 6

--- Class/account.py
+++ Class/account.py
@@ -4,6 +4,8 @@ import Class.user as user
 from fastapi import FastAPI, Depends, APIRouter, HTTPException
 from sqlmodel import Session, create_engine, Field, SQLModel
 from pydantic import BaseModel
+from datetime import datetime
+
 
 router = APIRouter(tags=["Accounts"])
 
@@ -17,6 +19,16 @@ class Account(SQLModel, BaseModel, table=True):
     name: str = Field(index=True, nullable=False )
     user_id: int = Field(index=True)
 
+class DepositTransaction(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    account_id: int = Field(index=True)
+    amount: float = Field(index=True)
+    created_at: datetime = Field(default=datetime.now())
+
+class Deposit(BaseModel):
+    account_id: int
+    amount: float
+    
 class CreateAccount(BaseModel):
     name: str
 
@@ -32,6 +44,27 @@ def create_account(body: CreateAccount, user_info=Depends(user.get_user), sessio
         return account
     raise HTTPException(401, "Please login")
 
+
+@router.post("/deposit/")
+def deposit_money(body: Deposit, user_info=Depends(user.get_user), session=Depends(database.get_session)):
+    if user_info:
+        if body.amount <= 0:
+            raise HTTPException(400, "The amount must be positive")
+        
+        account = session.get(Account, body.account_id)
+        if not account or account.user_id != user_info["id"]:
+            raise HTTPException(404, "account not found")
+        
+        account.balance += body.amount
+        session.add(account)
+        session.commit()
+        session.refresh(account)
+        
+        transaction = DepositTransaction(account_id=account.id, amount=body.amount, created_at=datetime.now())
+        session.add(transaction)
+        session.commit()
+        return {"message": "Deposit successfully completed.", "new_balance": account.balance}
+    raise HTTPException(401, "Please login")
 
 #*--------- App Get ----------#
 
